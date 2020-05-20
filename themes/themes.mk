7ea19fd3b99366f4de881c4a30f2fbd5edf389f1
Mrinal Ghosh <mg712702@gmail.com>
2020-05-20 18:03:28 +0530

    ColtSpares: Move themes to ColtSpares [2/2]
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

---
+++ themes/themes.mk
@@ -0,0 +1,14 @@
+# Colt Themes
+PRODUCT_PACKAGES += \
+    PitchBlackSystemOverlay \
+    PitchBlackSystemUIOverlay \
+    SolarizedDarkSystemOverlay \
+    SolarizedDarkSystemUIOverlay \
+    BakedGreenSystemOverlay \
+    BakedGreenSystemUIOverlay \
+    ChocoXSystemOverlay \
+    ChocoXSystemUIOverlay \
+    SystemDarkGrayOverlay \
+    SystemUIDarkGrayOverlay \
+    MaterialOceanSystemOverlay \
+    MaterialOceanSystemUIOverlay
