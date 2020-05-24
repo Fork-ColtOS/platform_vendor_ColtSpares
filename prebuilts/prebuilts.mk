9d3b70a9cb4143d8c1176ec8fc8801762c208aba
Mrinal Ghosh <mg712702@gmail.com>
2020-05-24 11:42:30 +0530

    ColtSpares: Remove prebuilt Markup & SoundPicker
    * The Opengapps Nano package already provides both of these,
      So why should we keep them here for those anti-google people's...
    
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- prebuilts/prebuilts.mk
+++ prebuilts/prebuilts.mk
@@ -19,8 +19,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/ColtSpares/prebuilts/etc/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lawnchair.xml \
     vendor/ColtSpares/prebuilts/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
-
-# Markup libs
-PRODUCT_COPY_FILES += \
-    vendor/ColtSpares/prebuilts/lib/libsketchology_native.so:system/product/lib/libsketchology_native.so \
-    vendor/ColtSpares/prebuilts/lib64/libsketchology_native.so:system/product/lib64/libsketchology_native.so
