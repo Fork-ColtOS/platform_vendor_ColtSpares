ff992a2ef73e5b8963aee7d7908a3206db0f6d66
Mrinal Ghosh <mg712702@gmail.com>
2020-08-23 18:35:22 +0530

    ColtSpares: prebuilts: Drop Lawnchair
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- prebuilts/prebuilts.mk
+++ prebuilts/prebuilts.mk
@@ -5,12 +5,6 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 # Colt packages
 PRODUCT_PACKAGES += \
     ColtPapers \
-    Lawnchair \
     QPGallery \
     SafetyHubPrebuilt \
     WeatherIcons
-
-# Lawnchair
-PRODUCT_COPY_FILES += \
-    vendor/ColtSpares/prebuilts/etc/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lawnchair.xml \
-    vendor/ColtSpares/prebuilts/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
