34d103a90749efc28640b10ba745c968cb267cda
Saurabh Charde <saurabhchardereal@gmail.com>
2020-08-23 16:35:21 +0000

    prebuilts: Add our own EnigmaLauncher
    * Special thanks to @spezi77
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- prebuilts/prebuilts.mk
+++ prebuilts/prebuilts.mk
@@ -5,6 +5,11 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 # Colt packages
 PRODUCT_PACKAGES += \
     ColtPapers \
+    EnigmaLauncher \
     QPGallery \
     SafetyHubPrebuilt \
     WeatherIcons
+
+PRODUCT_COPY_FILES += \
+    vendor/ColtSpares/prebuilts/etc/permissions/com.android.launcher3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.android.launcher3.xml \
+    vendor/ColtSpares/prebuilts/etc/permissions/com.android.launcher3-ext.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.android.launcher3-ext.xml
