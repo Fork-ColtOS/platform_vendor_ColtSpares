41f91dcaf6ca8a108ad03878bf7065346e509de8
rayrzy <vincentrayananda@gmail.com>
2022-04-20 16:13:51 +0700

    ColtSpares: drop some prebuilt apps
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: I965edcfa1ea83030d93e152e66586f1ecc521d78

--- prebuilts/prebuilts.mk
+++ prebuilts/prebuilts.mk
@@ -4,12 +4,5 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 # Colt packages
 PRODUCT_PACKAGES += \
-    ColtPapers \
-    EnigmaLauncher \
-    QPGallery \
     SafetyHubPrebuilt \
     WeatherIcons
-
-PRODUCT_COPY_FILES += \
-    vendor/ColtSpares/prebuilts/etc/permissions/com.enigma.launcher.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.enigma.launcher.xml \
-    vendor/ColtSpares/prebuilts/etc/permissions/com.enigma.launcher-ext.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.enigma.launcher-ext.xml
